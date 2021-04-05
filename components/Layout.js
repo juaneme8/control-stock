d52aee6261ff32c58f3fa99441516b8805bd0804
Juan Ocho <juaneme8@gmail.com>
2021-04-05 09:33:47 -0300

    Estilos Layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,10 @@
-import { Drawer, List, ListItem, ListItemIcon, ListItemText, makeStyles, Tooltip, Typography } from '@material-ui/core';
+import { AppBar, Drawer, IconButton, List, ListItem, ListItemIcon, ListItemText, makeStyles, Toolbar, Tooltip, Typography } from '@material-ui/core';
 import React from 'react';
 import Link from 'next/link';
 import ListIcon from '@material-ui/icons/List';
 const drawerWidth = 150;
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
 	root: {
 		display: 'flex',
 	},
@@ -14,7 +14,17 @@ const useStyles = makeStyles({
 	drawerPaper: {
 		width: drawerWidth,
 	},
-});
+	appbar: {
+		width: `calc(100% -  ${drawerWidth}px)`,
+		marginLeft: drawerWidth,
+	},
+	toolbar: theme.mixins.toolbar,
+	page: {
+		background: '#f9f9f9',
+		width: '100%',
+		padding: theme.spacing(3),
+	},
+}));
 
 function Layout({ children }) {
 	const classes = useStyles();
@@ -29,8 +39,12 @@ function Layout({ children }) {
 	];
 
 	return (
-		<div>
-			<div>app bar</div>
+		<div className={classes.root}>
+			<AppBar position='fixed' className={classes.appbar} elevation={0} color='primary'>
+				<Toolbar>
+					<Typography variant='h6'>CIME</Typography>
+				</Toolbar>
+			</AppBar>
 			<Drawer className={classes.drawer} variant='permanent' classes={{ paper: classes.drawerPaper }} anchor='left'>
 				<div>
 					<Typography variant='h5' className={classes.title}>
@@ -51,8 +65,10 @@ function Layout({ children }) {
 					))}
 				</List>
 			</Drawer>
-
-			<div>{children}</div>
+			<div className={classes.page}>
+				<div className={classes.toolbar}></div>
+				{children}
+			</div>
 		</div>
 	);
 }
