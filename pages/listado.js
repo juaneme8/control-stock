37ad36ea6b06cb3b71944f54034a6fa3f6d9f063
Juan Ocho <juaneme8@gmail.com>
2021-04-07 18:41:49 -0300

    Cambios en .prettierrc

--- pages/listado.js
+++ pages/listado.js
@@ -1,12 +1,10 @@
-import {
-  Avatar,
-  Card,
-  CardHeader,
-  Grid,
-  IconButton,
-  makeStyles,
-  TextField,
-} from '@material-ui/core';
+import Avatar from '@material-ui/core/Avatar';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import Grid from '@material-ui/core/Grid';
+import IconButton from '@material-ui/core/IconButton';
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core';
 import React, { useEffect, useState } from 'react';
 import { blue, deepPurple, green, lightBlue, red, yellow } from '@material-ui/core/colors';
 import axios from 'axios';
