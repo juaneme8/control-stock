e8844fe7605402248d2721ccf289d0b248cabe1f
Juan Ocho <juaneme8@gmail.com>
2021-04-19 13:48:25 -0300

    Cambios por linter

--- pages/api/devices/index.js
+++ pages/api/devices/index.js
@@ -1,13 +1,15 @@
-import dbConnect from '../../../utils/dbConnect';
 import Device from '../../../models/Device';
+import dbConnect from '../../../utils/dbConnect';
 
 export default async function handler(req, res) {
   const { method } = req;
+
   await dbConnect();
   switch (method) {
     case 'GET':
       try {
         const devices = await Device.find({});
+
         res.status(200).json({ success: true, data: devices });
       } catch (error) {
         res.status(400).json({ success: false });
@@ -16,6 +18,7 @@ export default async function handler(req, res) {
     case 'POST':
       try {
         const devices = await Device.create(req.body);
+
         res.status(201).json({ success: true, data: devices });
       } catch (error) {
         res.status(400).json({ success: false });
@@ -24,6 +27,7 @@ export default async function handler(req, res) {
     case 'DELETE':
       try {
         const devices = await Device.findByIdAndDelete(req.body._id);
+
         res.status(200).json({ success: true, data: devices });
       } catch (error) {
         res.status(400).json({ success: false });
