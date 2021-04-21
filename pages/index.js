549572522797195544645fac12dd5b097d59ceb9
Juan Ocho <juaneme8@gmail.com>
2021-04-21 14:02:16 -0300

    Agregado de AppBar y filtro de búsqueda

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,7 @@
+import { Flex, Input } from '@chakra-ui/react';
 import Head from 'next/head';
+
+import AppBar from '../components/AppBar';
 
 export default function Home() {
   return (
@@ -7,6 +10,9 @@ export default function Home() {
         <title>Control de Stock - CIME</title>
         <link href="/favicon.ico" rel="icon" />
       </Head>
+
+      <AppBar />
+      <Input maxWidth="1200" mt="10" p="10" placeholder="Buscar equipo" size="lg" />
     </>
   );
 }
