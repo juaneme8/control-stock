cbab45a7e81a460421c90235343bf9a8030a6904
Juan Ocho <juaneme8@gmail.com>
2021-04-21 14:01:45 -0300

    Switch de Color Mode

---
+++ components/DarkModeSwitch.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { MoonIcon, SunIcon } from '@chakra-ui/icons';
+import { useColorMode, IconButton } from '@chakra-ui/react';
+
+const DarkModeSwitch = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  return (
+    <IconButton
+      aria-label="Toggle Dark Switch"
+      icon={colorMode === 'dark' ? <SunIcon /> : <MoonIcon />}
+      onClick={toggleColorMode}
+    />
+  );
+};
+
+export default DarkModeSwitch;
