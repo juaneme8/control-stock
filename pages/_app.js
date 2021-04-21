3fd38436123727a62a386477bacb3313d1c33b68
Juan Ocho <juaneme8@gmail.com>
2021-04-21 13:59:04 -0300

    _app adaptado a Chakra

--- pages/_app.js
+++ pages/_app.js
@@ -1,42 +1,9 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import Head from 'next/head';
-import {ThemeProvider} from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-
-import theme from '../src/theme';
-import Layout from '../components/Layout';
-
-export default function MyApp({Component, pageProps}) {
-  React.useEffect(() => {
-    // Remove the server-side injected CSS.
-    const jssStyles = document.querySelector('#jss-server-side');
-
-    if (jssStyles) {
-      jssStyles.parentElement.removeChild(jssStyles);
-    }
-  }, []);
-
-  // console.log('testing with double braces');
-
+import { ChakraProvider } from '@chakra-ui/react';
+function MyApp({ Component, pageProps }) {
   return (
-    <React.Fragment>
-      <Head>
-        <title>Stock CIME</title>
-        <meta content="minimum-scale=1, initial-scale=1, width=device-width" name="viewport" />
-      </Head>
-      <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <CssBaseline />
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </ThemeProvider>
-    </React.Fragment>
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
   );
 }
-
-MyApp.propTypes = {
-  Component: PropTypes.elementType.isRequired,
-  pageProps: PropTypes.object.isRequired,
-};
+export default MyApp;
