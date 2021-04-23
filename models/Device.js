ebc9f414662e4f9bbffb4feb047f1c57f17ad4d7
Juan Ocho <juaneme8@gmail.com>
2021-04-22 23:14:54 -0300

    ✨ Agregado de timestamp en model

--- models/Device.js
+++ models/Device.js
@@ -1,8 +1,11 @@
 import mongoose from 'mongoose';
 
-const DeviceSchema = new mongoose.Schema({
-  name: String,
-  line: String,
-});
+const DeviceSchema = new mongoose.Schema(
+  {
+    name: String,
+    line: String,
+  },
+  { timestamps: true }
+);
 
 export default mongoose.models.Device || mongoose.model('Device', DeviceSchema);
