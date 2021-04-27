d99b141fbd5d2d1024a8879102e171ebea601a3a
Juan Ocho <juaneme8@gmail.com>
2021-04-27 17:22:27 -0300

    ✨ Agregado de badge con el estado del equipo

--- components/Device.js
+++ components/Device.js
@@ -1,16 +1,22 @@
-import { Circle, Text, VStack } from '@chakra-ui/layout';
+import { HStack, Flex, Badge, Circle, Text, VStack } from '@chakra-ui/layout';
 
 const Device = ({ device }) => {
-  const { barcode, name, description, line, location, manufacturer } = device;
+  const { state, barcode, name, description, line, location, manufacturer } = device;
 
   console.log(device);
 
   return (
     <VStack color="teal.800" spacing={1}>
-      <Circle bg="teal.800" color="white" size="40px">
-        <Text color="teal.100"> {line}</Text>
-      </Circle>
-      <Text fontSize="sm">{barcode}</Text>
+      <Badge colorScheme={state} mb={2} variant="solid">
+        RECHAZADO
+      </Badge>
+      <HStack>
+        <Circle bg="teal.800" color="white" size="40px">
+          <Text color="teal.100"> {line}</Text>
+        </Circle>
+        <Text fontSize="sm">{barcode}</Text>
+      </HStack>
+
       <Text casing="uppercase">{name}</Text>
       <Text>{description}</Text>
       <Text>{location}</Text>
