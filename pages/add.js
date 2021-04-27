3e7cbfea82be48f185d1362d40374eb60b2759fe
Juan Ocho <juaneme8@gmail.com>
2021-04-27 14:02:19 -0300

    ✨ Componente de página para agregar equipos

--- pages/add.js
+++ pages/add.js
@@ -1,11 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const add = () => {
+import { Button } from '@chakra-ui/button';
+import { Input } from '@chakra-ui/input';
+import { Flex, Heading } from '@chakra-ui/layout';
+import { BiPlus } from 'react-icons/bi';
+
+import DeviceDetails from '../components/DeviceDetails';
+
+const Add = () => {
+  const [searchInput, setSearchInput] = useState('');
+  const [showDetails, setShowDetails] = useState(false);
+
+  const handleInputChange = (e) => {
+    // Cambió el barcode entonces limpio detalles
+    setShowDetails(false);
+    const {
+      target: { value: inputValue },
+    } = e;
+
+    setSearchInput(inputValue);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    setShowDetails(true);
+    // Si el código ingresado tiene datos en la DB los muestro y sino coloco un formulario para que los actualice.
+    // console.log(searchInput);
+  };
+
   return (
-    <div>
-      <h1>add</h1>
-    </div>
+    <>
+      <Heading as="h2" size="lg">
+        Scanear Equipos
+      </Heading>
+      <form onSubmit={handleSubmit}>
+        <Flex mt={4}>
+          <Input autoFocus placeholder="Agregar Equipo" size="lg" value={searchInput} onChange={handleInputChange} />
+          <Button disabled={!searchInput} ml="2" size="lg" type="submit">
+            <BiPlus />
+          </Button>
+        </Flex>
+      </form>
+
+      {showDetails && <DeviceDetails barcode={searchInput} />}
+    </>
   );
 };
 
-export default add;
+export default Add;
