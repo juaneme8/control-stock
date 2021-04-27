05f4b0f0b10fd6fc00227776eb4ea7fdd334fe82
Juan Ocho <juaneme8@gmail.com>
2021-04-27 14:12:01 -0300

    🔥 Se eliminan timestamps

--- models/Device.js
+++ models/Device.js
@@ -1,15 +1,12 @@
 import mongoose from 'mongoose';
 
-const DeviceSchema = new mongoose.Schema(
-  {
-    barcode: { type: String, required: true },
-    name: { type: String, required: true },
-    description: { type: String, required: false },
-    line: { type: String, required: false },
-    manufacturer: { type: String, required: false },
-    location: { type: String, required: false },
-  },
-  { timestamps: true }
-);
+const DeviceSchema = new mongoose.Schema({
+  barcode: { type: String, required: true },
+  name: { type: String, required: true },
+  description: { type: String, required: false },
+  line: { type: String, required: false },
+  manufacturer: { type: String, required: false },
+  location: { type: String, required: false },
+});
 
 export default mongoose.models.Device || mongoose.model('Device', DeviceSchema);
