9898623a8fb5d256e76dbdaac283a1262d3a1d4b
Juan Ocho <juaneme8@gmail.com>
2021-04-27 08:49:04 -0300

    💄 Agregado de layout en root

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,26 @@
-import { ChakraProvider } from '@chakra-ui/react';
+import { Box, Button, ChakraProvider, Container, Flex, HStack } from '@chakra-ui/react';
+import Link from 'next/link';
+
+import DarkModeSwitch from '../components/DarkModeSwitch';
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
-      <Component {...pageProps} />
+      <Box padding={4}>
+        <Container borderRadius="sm" boxShadow="md" maxWidth="container.xl" padding={4}>
+          <Flex justifyContent="space-between" mb={4}>
+            <HStack spacing={4}>
+              <Button colorScheme="teal" variant="outline">
+                <Link href="/list">Listado</Link>
+              </Button>
+              <Button colorScheme="teal" variant="outline">
+                <Link href="/add">Agregar</Link>
+              </Button>
+            </HStack>
+            <DarkModeSwitch />
+          </Flex>
+          <Component {...pageProps} />
+        </Container>
+      </Box>
     </ChakraProvider>
   );
 }
