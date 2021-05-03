8a590905856e5065b5b8359a816108272134c63e
Juan Ocho <juaneme8@gmail.com>
2021-05-03 09:23:14 -0300

    ✨ Incorporación de radio buttons de state

--- components/DeviceDetails.js
+++ components/DeviceDetails.js
@@ -1,9 +1,6 @@
 import React, { useEffect, useState } from 'react';
 
-import { Button } from '@chakra-ui/button';
-import { FormControl, FormLabel } from '@chakra-ui/form-control';
-import { Input } from '@chakra-ui/input';
-import { Badge, Box, Center } from '@chakra-ui/layout';
+import { Button, FormControl, FormLabel, Input, Badge, Box, Center, HStack, Radio, RadioGroup } from '@chakra-ui/react';
 import axios from 'axios';
 
 const DeviceDetails = ({ barcode }) => {
@@ -17,8 +14,9 @@ const DeviceDetails = ({ barcode }) => {
 
       // Si el get fue exitoso
       if (data.success) {
-        // console.table(data.data);
-        setDevice(data.data);
+        // Si el código buscado no existe obtengo null
+        if (!data.data) setDevice({});
+        else setDevice(data.data);
       } else {
         setDevice({});
       }
@@ -30,6 +28,10 @@ const DeviceDetails = ({ barcode }) => {
   const handleInputChange = (e) => {
     setDevice({ ...device, [e.target.name]: e.target.value });
   };
+  const handleRadioChange = (e) => {
+    // console.log(e);
+    setDevice({ ...device, state: e });
+  };
 
   const handleSaveDevice = () => {
     const saveDevice = async () => {
@@ -39,6 +41,7 @@ const DeviceDetails = ({ barcode }) => {
         line: device.line,
         manufacturer: device.manufacturer,
         location: device.location,
+        state: device.state,
       });
 
       const { data } = res;
@@ -56,31 +59,55 @@ const DeviceDetails = ({ barcode }) => {
     saveDevice();
   };
 
+  console.log(device);
+
   return (
     <Box bg="gray.50" border="1px" borderColor="gray.300" borderRadius="md" mt={8} p={4}>
       <Badge colorScheme="teal" mb={4} variant="solid">
         Código de Barras: {barcode}
       </Badge>
-      <FormControl id="name" mb={4}>
-        <FormLabel>Nombre Equipo</FormLabel>
+
+      <FormControl as="fieldset" id="state" mb={4}>
+        <FormLabel as="legend">Estado</FormLabel>
+        <RadioGroup value={device?.state || `fix`} onChange={handleRadioChange}>
+          <HStack spacing={4}>
+            <Radio name="state" value="approved">
+              Aprobado
+            </Radio>
+            <Radio name="state" value="fix">
+              Para Intervenir
+            </Radio>
+            <Radio name="state" value="rejected">
+              Rechazado
+            </Radio>
+          </HStack>
+        </RadioGroup>
+      </FormControl>
+
+      <FormControl as="fieldset" id="name" mb={4}>
+        <FormLabel as="legend">Nombre Equipo</FormLabel>
         <Input name="name" value={device?.name || ``} onChange={handleInputChange} />
       </FormControl>
+
       <FormControl id="description" mb={4}>
-        <FormLabel>Descripción</FormLabel>
+        <FormLabel as="legend">Descripción</FormLabel>
         <Input name="description" value={device?.description || ``} onChange={handleInputChange} />
       </FormControl>
       <FormControl id="line" mb={4}>
-        <FormLabel>Línea</FormLabel>
+        <FormLabel as="legend">Línea</FormLabel>
         <Input name="line" value={device?.line || ``} onChange={handleInputChange} />
       </FormControl>
+
       <FormControl id="manufacturer" mb={4}>
-        <FormLabel>Flota</FormLabel>
+        <FormLabel as="legend">Flota</FormLabel>
         <Input name="manufacturer" value={device?.manufacturer || ``} onChange={handleInputChange} />
       </FormControl>
+
       <FormControl id="location" mb={4}>
-        <FormLabel>Ubicación</FormLabel>
+        <FormLabel as="legend">Ubicación</FormLabel>
         <Input name="location" value={device?.location || ``} onChange={handleInputChange} />
       </FormControl>
+
       <Center>
         <Button colorScheme="teal" variant="outline" onClick={handleSaveDevice}>
           {device ? 'Actualizar' : 'Crear Nuevo'}
