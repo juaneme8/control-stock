09c396eb7563681ad4b6aee262b879054acff62c
Juan Ocho <juaneme8@gmail.com>
2021-05-03 08:04:31 -0300

    ♻️ Agregado de componente Layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,35 +1,13 @@
-import { Box, Button, ChakraProvider, Container, Flex, HStack } from '@chakra-ui/react';
-import Link from 'next/link';
+import { ChakraProvider } from '@chakra-ui/react';
 
-import DarkModeSwitch from '../components/DarkModeSwitch';
+import Layout from '../components/Layout';
+
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
-      <Box padding={4}>
-        <Container borderRadius="sm" boxShadow="md" height="100vh" maxWidth="container.xl" padding={4}>
-          <Flex justifyContent="space-between" mb={4}>
-            <HStack spacing={4}>
-              <Link href="/">
-                <Button colorScheme="teal" variant="outline">
-                  Inicio
-                </Button>
-              </Link>
-              <Link href="/list">
-                <Button colorScheme="teal" variant="outline">
-                  Listado Equipos
-                </Button>
-              </Link>
-              <Link href="/add">
-                <Button colorScheme="teal" variant="outline">
-                  Scanear Equipos
-                </Button>
-              </Link>
-            </HStack>
-            <DarkModeSwitch />
-          </Flex>
-          <Component {...pageProps} />
-        </Container>
-      </Box>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </ChakraProvider>
   );
 }
