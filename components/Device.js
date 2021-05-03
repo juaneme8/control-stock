66d775d9d16c2a2037154c85ae2a82aad5a394d0
Juan Ocho <juaneme8@gmail.com>
2021-05-03 12:11:21 -0300

    💄 Cambios en maqueta de card

--- components/Device.js
+++ components/Device.js
@@ -1,26 +1,28 @@
-import { HStack, Badge, Circle, Text, VStack } from '@chakra-ui/layout';
+import { Text, VStack } from '@chakra-ui/layout';
 
 const Device = ({ device }) => {
-  const { state, barcode, name, description, line, location, manufacturer } = device;
+  const { barcode, name, description, line, location, manufacturer } = device;
 
   console.log(device);
 
+  const getLineColor = () => {
+    if (line === 'A') return 'blue.100';
+    if (line === 'B') return 'red.100';
+    if (line === 'C') return 'blue.400';
+  };
+
   return (
     <VStack color="teal.800" spacing={1}>
-      <Badge colorScheme={state} mb={2} variant="solid">
-        RECHAZADO
-      </Badge>
-      <HStack>
-        <Circle bg="teal.800" color="white" size="40px">
-          <Text color="teal.100"> {line}</Text>
-        </Circle>
-        <Text fontSize="sm">{barcode}</Text>
-      </HStack>
-
-      <Text casing="uppercase">{name}</Text>
+      <Text casing="uppercase" fontWeight="bold">
+        {name}
+      </Text>
       <Text>{description}</Text>
       <Text>{location}</Text>
       <Text>{manufacturer}</Text>
+      <Text bg={getLineColor()} borderRadius="md" color="black.100" px={2}>
+        LINEA {line}{' '}
+      </Text>
+      <Text fontSize="sm">{barcode}</Text>
     </VStack>
   );
 };
