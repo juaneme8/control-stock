a956b940a9d63a661bea608c079031a113f254cb
Juan Ocho <juaneme8@gmail.com>
2021-08-25 13:27:37 -0300

    Cambios en maqueta listado

--- pages/list.js
+++ pages/list.js
@@ -8,12 +8,19 @@ import Link from 'next/link';
 import { BiSearchAlt2 } from 'react-icons/bi';
 
 import Device from '../components/Device';
+import { Box } from '@chakra-ui/react';
 
 function List() {
   const [devices, setDevices] = useState([]);
   const [showingDevices, setShowingDevices] = useState([]);
   const [searchInput, setSearchInput] = useState('');
 
+  const getStateColor = (state) => {
+    if (state === 'approved') return 'green.400';
+    if (state === 'fix') return 'yellow.400';
+    if (state === 'rejected') return 'red.400';
+  };
+  
   useEffect(() => {
     const fetchDevices = async () => {
       const res = await axios.get('http://localhost:3001/api/devices/');
@@ -48,11 +55,7 @@ function List() {
     setShowingDevices(auxDevices);
   };
 
-  const getStateColor = (state) => {
-    if (state === 'approved') return 'green.400';
-    if (state === 'fix') return 'yellow.400';
-    if (state === 'rejected') return 'red.400';
-  };
+
 
   return (
     <>
@@ -72,16 +75,17 @@ function List() {
       <Grid gap={4} mt="6" templateColumns="repeat(auto-fill, minmax(200px,1fr))">
         {showingDevices.map((device) => (
           <GridItem key={device.id} as={Link} href={`/devices/${device.barcode}`}>
-            <Center
+            <Box
               bg="gray.50"
               border="1px"
-              borderColor={getStateColor(device.state)}
+              // borderColor={getStateColor(device.state)}
+              borderColor="gray.400"
               cursor="pointer"
               p={4}
               rounded="lg"
             >
               <Device device={device} />
-            </Center>
+            </Box>
           </GridItem>
         ))}
       </Grid>
