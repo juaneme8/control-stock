178365aa7fba6a58385e213f12d9d5b96fd38aed
Juan Ocho <juaneme8@gmail.com>
2021-08-25 13:25:57 -0300

    Cambios en maqueta de dispositivo

--- components/Device.js
+++ components/Device.js
@@ -1,31 +1,46 @@
 import { Text, VStack } from '@chakra-ui/layout';
+import { Box, Circle, Flex, HStack, Square, Stack } from '@chakra-ui/react';
 
 const Device = ({ device }) => {
-  const { barcode, name, description, line, location, manufacturer } = device;
+  const { barcode, description, code, serie, brand, location, state, image, catalogue } = device;
 
   const getLineColor = () => {
-    if (line === 'A') return 'blue.100';
-    if (line === 'B') return 'red.100';
-    if (line === 'C') return 'blue.400';
-    if (line === 'D') return 'green.400';
-    if (line === 'E') return 'purple.400';
-    if (line === 'H') return 'yellow.500';
-    if (line === 'P') return 'yellow.800';
+    if (location === 'Linea A') return 'blue.100';
+    if (location === 'Linea B') return 'red.100';
+    if (location === 'Linea C') return 'blue.400';
+    if (location === 'Linea D') return 'green.400';
+    if (location === 'Linea E') return 'purple.400';
+    if (location === 'Linea H') return 'yellow.500';
+    if (location === 'Linea P') return 'yellow.800';
+    if (location === 'CIME') return 'teal.400';
+  };
+
+  const getStateColor = (state) => {
+    if (state === 'approved') return 'green.400';
+    if (state === 'fix') return 'yellow.400';
+    if (state === 'rejected') return 'red.400';
   };
 
   return (
-    <VStack color="teal.800" spacing={1}>
-      <Text casing="uppercase" fontWeight="bold">
-        {name}
+    <Box color="gray.600">
+      <Flex align="center">
+      <Circle size="10px" bg={getStateColor(state)} color="white"></Circle> 
+        <Text bg="gray.200" borderRadius="md" px={1} fontSize="sm" align="center" flex="1" ml={2}>{barcode}</Text>
+      </Flex>
+
+      <Text casing="uppercase" fontWeight="bold" noOfLines={1}>
+        {description}
       </Text>
-      <Text>{description}</Text>
-      <Text>{location}</Text>
-      <Text>{manufacturer}</Text>
-      <Text bg={getLineColor()} borderRadius="md" color="black.100" px={2}>
-        LINEA {line}{' '}
+
+      <Text bg={getLineColor()} borderRadius="md" color="white" px={2}>
+        {location}
       </Text>
-      <Text fontSize="sm">{barcode}</Text>
-    </VStack>
+     
+      <Text mt={2}>{brand}</Text>
+      <Text>{code}</Text>
+      <Text>{serie}</Text>
+      
+    </Box>
   );
 };
 
