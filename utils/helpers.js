5e429003d5ccad077da76a33b910cb13a2a65f43
Juan Ocho <juaneme8@gmail.com>
2021-09-06 10:10:51 -0300

    Cambio en los colores de los talleres

--- utils/helpers.js
+++ utils/helpers.js
@@ -5,12 +5,28 @@ export const getStateColor = state => {
 };
 
 export const getLineColor = (location) => {
-    if (location === 'Linea A') return 'blue.100';
-    if (location === 'Linea B') return 'red.400';
-    if (location === 'Linea C') return 'blue.400';
-    if (location === 'Linea D') return 'green.400';
-    if (location === 'Linea E') return 'purple.400';
-    if (location === 'Linea H') return 'yellow.500';
-    if (location === 'Linea P') return 'yellow.800';
     if (location === 'CIME') return 'teal.400';
+
+    if (location === 'Taller Nazca') return 'blue.300';
+    if (location === 'Taller Polvorín') return 'blue.300';
+
+    if (location === 'Taller Urquiza') return 'red.400';
+    if (location === 'Taller Rancagua') return 'red.400';
+
+    if (location === 'Taller Constitución') return 'blue.500';
+
+    if (location === 'Taller Canning') return 'green.400';
+    if (location === 'Taller Congreso de Tucumán') return 'green.400';
+    
+    if (location === 'San José') return 'purple.400';
+    
+    if (location === 'Taller Colonia') return 'yellow.500';
+    if (location === 'Taller Parque Patricios') return 'yellow.500';
+
+
+    if (location === 'Taller Rubén Darío') return 'orange.500';
+    
+    if (location === 'Taller Mariano Acosta') return 'yellow.700';
+
+    else return 'gray.500'
   };
